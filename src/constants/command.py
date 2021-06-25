77f0a6229bc7398d9183fdc368b0e5808e563528
Nepumi-Jr <peerator@kkumail.com>
2021-06-25 20:55:25 +0700

    🚨C11

--- src/constants/command.py
+++ src/constants/command.py
@@ -2,13 +2,13 @@ ioeredirect = " 2>env/error.txt"
 langarr = {
     "c": {
         "extension": "c",
-        "compile": "gcc [sourcePath] -O2 -std=c17 -fomit-frame-pointer -o env/out"
+        "compile": "gcc [sourcePath] -O2 -std=c11 -fomit-frame-pointer -o env/out"
         + ioeredirect,
         "execute": "env/out[inputfile]",
     },
     "cpp": {
         "extension": "cpp",
-        "compile": "g++ [sourcePath] -O2 -std=c++17 -fomit-frame-pointer -o env/out"
+        "compile": "g++ [sourcePath] -O2 -std=c++11 -fomit-frame-pointer -o env/out"
         + ioeredirect,
         "execute": "env/out[inputfile]",
     },
