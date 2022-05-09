fff5fedc8d2b3ffa384b72c87f895440d2573f70
Nepumi-Jr <peerator@kkumail.com>
2022-05-09 18:14:16 +0700

    :beers: try to post in httpQuery

--- src/httpSync/httpQuery.py
+++ src/httpSync/httpQuery.py
@@ -1,8 +1,9 @@
 import requests
+import time
 
 from DTO.result import ResultDTO
 from constants.osDotEnv import osEnv
-
+from message import *
 
 def getUrl(path):
     return f'{osEnv.SYNC_HOST}:{osEnv.SYNC_PORT}/{path}'
@@ -22,7 +23,7 @@ def updateRunningInCase(resultId, case):
 
 
 def updateResult(result: ResultDTO):
-    # TODO : Imprement memUse in DB
+    # TODO : Implement memUse in DB
     status = "accept" if all(
         c in "P[]()" for c in result.result) or result.result == "Accepted" else "reject"
     body = {
@@ -33,4 +34,21 @@ def updateResult(result: ResultDTO):
         "status": status,
         "errmsg": result.errmsg,
     }
-    requests.post(getUrl(f'result/{result.id}'), json=body)
+    
+    tries = 0
+    while True:
+        tries += 1
+        printHeader("HTTP", f"Try to post back to grader ({tries} tries)")
+        try:
+            postResult = requests.post(getUrl(f'result/{result.id}'), json=body)
+        except:
+            printFail("HTTP", "Fail to Post... retrying")
+            time.sleep(2)
+            continue
+        
+        if postResult.status_code != 200:
+            printFail("HTTP", f"Fail to Post with code ({postResult.status_code})... retrying")
+            time.sleep(2)
+            continue
+        else:
+            printOKGreen("HTTP", "Posted and Done")
