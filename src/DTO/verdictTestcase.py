e67f54f91c620c5aa9d61da895e3b0792e9152e2
ttamx <teetat.tata@gmail.com>
2025-01-16 10:52:42 +0700

    update fullresult & memuse to database

--- src/DTO/verdictTestcase.py
+++ src/DTO/verdictTestcase.py
@@ -3,7 +3,7 @@ from constants.Enums import VerdictStatus
 
 
 @dataclass
-class sVerdictTestcase:
+class VerdictTestcase:
     status: VerdictStatus
     percent: float
     # ? from 0 to 1 (between is accepted e.g. 0.69)
